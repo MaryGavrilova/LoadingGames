47cab2a75b28e0eb8156083b4fbbab9ee1775fb6
Maria Gavrilova <zuzina_mary@mail.ru>
2021-05-30 12:05:23 +0300

    Second version

--- src/Main.java
+++ src/Main.java
@@ -21,10 +21,10 @@ public class Main {
     public static void openZip(String zipPath, String extractPath) {
         try (ZipInputStream zin = new ZipInputStream(new FileInputStream(zipPath))) {
             ZipEntry entry;
-            String name;
+            String nameOfFile;
             while ((entry = zin.getNextEntry()) != null) {
-                name = entry.getName();
-                FileOutputStream fout = new FileOutputStream(extractPath + name);
+                nameOfFile = entry.getName();
+                FileOutputStream fout = new FileOutputStream(extractPath + nameOfFile);
                 for (int c = zin.read(); c != -1; c = zin.read()) {
                     fout.write(c);
                 }
